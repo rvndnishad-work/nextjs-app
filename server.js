78f5d2e9717cabb3a968e89bfe4e744d88d579ac
Arvind Kumar Nishad <Arvind_Kumar_Nishad@external.mckinsey.com>
2019-05-12 23:12:44 +0530

    Menubar set

--- server.js
+++ server.js
@@ -10,6 +10,17 @@ app
   .then(() => {
     const server = express();
 
+    const path = require("path");
+    const options = {
+      root: path.join(__dirname, "/static"),
+      headers: {
+        "Content-Type": "text/plain;charset=UTF-8"
+      }
+    };
+    server.get("/robots.txt", (req, res) =>
+      res.status(200).sendFile("robots.txt", options)
+    );
+
     server.get("*", (req, res) => {
       return handle(req, res);
     });
