f94458c5120bc94326b71e92db8e7b8fdb6631f7
Arvind Kumar Nishad <Arvind_Kumar_Nishad@external.mckinsey.com>
2019-05-12 06:13:04 +0530

    index updated

--- components/head.js
+++ components/head.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import NextHead from 'next/head'
-import { string } from 'prop-types'
+import React from "react";
+import NextHead from "next/head";
+import { string } from "prop-types";
 
-const defaultDescription = ''
-const defaultOGURL = ''
-const defaultOGImage = ''
+const defaultDescription = "";
+const defaultOGURL = "";
+const defaultOGImage = "";
 
 const Head = props => (
   <NextHead>
     <meta charSet="UTF-8" />
-    <title>{props.title || ''}</title>
+    <title>{props.title || ""}</title>
     <meta
       name="description"
       content={props.description || defaultDescription}
@@ -20,8 +20,11 @@ const Head = props => (
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#49B882" />
     <link rel="icon" href="/static/favicon.ico" />
     <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta name="google-site-verification" content="1lWHSm6U2pAykkfTxNDeX6UtRWeSjDD9gJRanRathwU" />
-    <meta property="og:title" content={props.title || ''} />
+    <meta
+      name="google-site-verification"
+      content="1lWHSm6U2pAykkfTxNDeX6UtRWeSjDD9gJRanRathwU"
+    />
+    <meta property="og:title" content={props.title || ""} />
     <meta
       property="og:description"
       content={props.description || defaultDescription}
@@ -33,13 +36,13 @@ const Head = props => (
     <meta property="og:image:width" content="1200" />
     <meta property="og:image:height" content="630" />
   </NextHead>
-)
+);
 
 Head.propTypes = {
   title: string,
   description: string,
   url: string,
   ogImage: string
-}
+};
 
-export default Head
+export default Head;
